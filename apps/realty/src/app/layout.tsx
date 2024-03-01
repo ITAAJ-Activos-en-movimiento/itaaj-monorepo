17f008be7d65dd2ab69cc145fb2432fd8a8448c1
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-02-29 20:54:09 -0500

    chore: finished

--- apps/realty/src/app/layout.tsx
+++ apps/realty/src/app/layout.tsx
@@ -6,8 +6,11 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Itaaj Realty: compra y venta de departamentos',
+  metadataBase: new URL("https://itaajrealty.com"),
+  title: 'Itaaj Realty: compra y venta de inmebles en México',
   description: 'Más de 1.500.000 departamentos en venta, casas y oficinas nuevos o de segunda mano. Vende o compra tu departamento o casa en Itaaj Realty completamente GRATIS!"',
+  applicationName: "Itaaj Realty",
+  keywords: ["Inmuebles", "Propiedades", "Departamentos", "Casas", "Lofts"]
 }
 
 export default function RootLayout({
