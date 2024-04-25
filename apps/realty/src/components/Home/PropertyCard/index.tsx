17dd12e5757dc0cd468a992df514ddace8bdbab4
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-04-25 10:42:55 -0500

    chore: add

--- apps/realty/src/components/Home/PropertyCard/index.tsx
+++ apps/realty/src/components/Home/PropertyCard/index.tsx
@@ -11,7 +11,7 @@ const ProeprtyCard = ({loc, images, city, address, name, price, category, slug, 
         <picture className={styles.picture}>
 
         <Image src={images && images?.length > 2? images[0] : '/img-placeholder.jpg'} width={1050} height={150} style={{
-            objectFit: 'cover'
+            objectFit: 'contain'
         }} alt={name} /> 
         </picture>
 
