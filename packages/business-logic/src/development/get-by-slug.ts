d0cd146affe38c81c9784bad34932fd509d6d52b
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-04-29 21:55:29 -0500

    chore: new infrea

--- packages/business-logic/src/development/get-by-slug.ts
+++ packages/business-logic/src/development/get-by-slug.ts
@@ -2,16 +2,34 @@ import { getDbInstance } from '@itaaj/data-sources/src/postgresql';
 import { Development, developments} from '@itaaj/entities';
 import { and, eq } from 'drizzle-orm';
 
+function detectUuidOrSlug(input: string): 'uuid' | 'slug' | 'unknown' {
+     const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+     const slugRegex = /^[a-zA-Z0-9_-]{3,}$/;
+ 
+     if (uuidRegex.test(input)) {
+         return 'uuid';
+     } else if (slugRegex.test(input)) {
+         return 'slug';
+     } else {
+         return 'slug';
+     }
+ }
+ 
+
 export const getDevelopmentById = async (slug: string): Promise<Development> => {
     
-     let result = await getDbInstance().select().from(developments)
-     .where(and(eq(developments.slug, slug!)));
+     const type = detectUuidOrSlug(slug);
+
+     let result;
      
-     if(!result){
+     if(type == "uuid"){
           result = await getDbInstance().select().from(developments)
           .where(and(eq(developments.id, slug!)));
+     }else{
+          result = await getDbInstance().select().from(developments)
+     .where(and(eq(developments.slug, slug!)));
      }
-    
+     
      const development = result[0] as Development;
     
      return development;
