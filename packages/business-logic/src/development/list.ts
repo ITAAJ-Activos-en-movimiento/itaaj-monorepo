7decc4bae929011e2b6873bfdfd468e6768d99cb
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-04-30 21:06:18 -0500

    chore: new info

--- packages/business-logic/src/development/list.ts
+++ packages/business-logic/src/development/list.ts
@@ -37,31 +37,5 @@ export const getAllDevelopments = async () => {
       blockchainId: developments.blockchainId,
     })
     .from(developments);
-
-
-    result.map(async (data: any) => {
-      const code = generatePropertyCode();
-      if (!data.blockchainId) {
-        await getDbInstance()
-          .update(developments)
-          .set({...data, blockchainId: code })
-          .where(eq(developments.id, data.id || ""))
-          .returning();
-      }
-    });
   return result;
 };
-
-
-function generatePropertyCode(): string {
-  const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  const numeros = '0123456789';
-  const letraInicial = letras.charAt(Math.floor(Math.random() * letras.length));
-  let codigo = letraInicial;
-  
-  for (let i = 0; i < 5; i++) { // Generar 5 dígitos numéricos después de la letra
-      codigo += numeros.charAt(Math.floor(Math.random() * numeros.length));
-  }
-  
-  return codigo;
-}
