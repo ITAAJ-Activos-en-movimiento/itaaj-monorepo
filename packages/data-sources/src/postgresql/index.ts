7d127308d0eba0568b9d9aa1a1d978873e61b8db
matosr96 <19matos96@gmail.com>
2024-07-25 09:03:51 -0500

    added .env conection of postgres aws

--- packages/data-sources/src/postgresql/index.ts
+++ packages/data-sources/src/postgresql/index.ts
@@ -1,27 +1,31 @@
-import { Pool } from 'pg';
-import { NodePgDatabase, drizzle } from  'drizzle-orm/node-postgres';
-import { migrate } from  'drizzle-orm/node-postgres/migrator';
+import { Pool } from "pg";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 
+const { HOSTDB, PORTDB, USERDB, PASSWORDDB, DATABASE } = process.env;
 export interface InitPostgresOptions {
-    url?: string;
+  url?: string;
 }
 
-let dbInstace: any; 
-export const initPostgres = async ({url}: InitPostgresOptions) => {
+let dbInstace: any;
+export const initPostgres = async ({ url }: InitPostgresOptions) => {
+  const pool = new Pool({
+    host: HOSTDB,
+    port: Number(PORTDB),
+    user: USERDB,
+    password: PASSWORDDB,
+    database: DATABASE,
+    ssl: { rejectUnauthorized: false },
+  });
 
-    const pool = new Pool({
-        connectionString: url,
-        ssl: true
-    });
+  const db = drizzle(pool);
 
-    const db = drizzle(pool);
+  await migrate(db, {
+    migrationsFolder: "./migrations",
+  });
 
-    await migrate(db, {
-        migrationsFolder: './migrations'
-    });
-
-    dbInstace = db;
-    console.info(`Postgres connection`)
-}
+  dbInstace = db;
+  console.info(`Postgres connection`);
+};
 
 export const getDbInstance = () => dbInstace;
