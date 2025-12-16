5d5c17caf1a191b3077e4b4cd6619d851a4e0cb2
Nestor Mosquera <yunsde26@gmail.com>
2025-12-15 22:58:02 -0500

    chore: new ref

--- apps/api/src/server/index.ts
+++ apps/api/src/server/index.ts
@@ -10,7 +10,8 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://itaajrealty.com"
+    "https://itaajrealty.com",
+    "https://www.itaajrealty.com"
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
--- apps/realty/src/modules/auth/hooks/useLogout.ts
+++ apps/realty/src/modules/auth/hooks/useLogout.ts
@@ -12,7 +12,7 @@ export const useLogout = () => {
 
     setLoading(true);
     try {
-      await fetch("http://localhost:8000/api/v1/auth/logout", {
+      await fetch("https://itaaj-realty.onrender.com/api/v1/auth/logout", {
         method: "POST",
         credentials: "include",
       });
